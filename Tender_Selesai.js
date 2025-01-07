3dbd230169c1204d0c001adf8cde07dc21afa408
benmsvgr-ai <benrama0489@gmail.com>
2025-01-07 19:36:55 +0700

    Update Tender_Selesai.js

--- Tender_Selesai.js
+++ Tender_Selesai.js
@@ -16,8 +16,8 @@ const url = process.env.API_URL_TS; // Use environment variable for URL
 
 // Define the spreadsheet ID and range
 const spreadsheetId = process.env.SPREADSHEET_ID; // Use environment variable for Spreadsheet ID
-const clearRange = 'Tender!A:ZZ'; // Range to clear
-const updateRange = 'Tender!A1'; // Range to update
+const clearRange = 'Swakelola!A:ZZ'; // Range to clear
+const updateRange = 'Swakelola!A1'; // Range to update
 
 async function fetchData() {
     try {
